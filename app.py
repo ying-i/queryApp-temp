2abae86ba204c3cbf2edccdc18f65954c87af96d
ying-i <133798280+ying-i@users.noreply.github.com>
2024-03-12 17:07:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def index():
         os.remove(image_path)
 
         return render_template("index.html",result=result)
-    return render_template("index.html")
+    return render_template("index.html", result=result)
     #Flask automatically looks for templates in the templates folder,
     #when you call the render_template() function in your Flask application.
 
